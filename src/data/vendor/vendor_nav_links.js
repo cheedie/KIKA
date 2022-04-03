48155996192385c57dedd885e6b7761847c62d62
Ifechi <iamifechi@gmail.com>
2022-04-03 22:41:34 +0100

    vendor dashboard genesis

---
+++ src/data/vendor/vendor_nav_links.js
@@ -0,0 +1,9 @@
+export const vendor_nav_links =[
+    {title: "Dashboard", link:"/", active:true},
+    {title: "Products", link:"/products"},
+    {title: "Orders", link:"/orders"},
+    {title: "Report", link:"/report"},
+    {title: "Review", link:"/review"},
+    {title: "Withdraw", link:"/withdraw"},
+    {title: "Settings", link:"/settings"},
+] ;
