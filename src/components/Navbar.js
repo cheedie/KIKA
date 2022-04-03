71c8a4a0bb5ec9857317da9c867346b68e78bd5a
Martin Karugaba <martinkarugaba21@gmail.com>
2022-04-04 02:19:09 +0300

    martin initial changes

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar__logo">
+        <h1>KIKA</h1>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Navbar;
