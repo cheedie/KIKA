8abffdffcf2ad46999c584f101ca57cb051ef38a
cheedieebeogu <cheedie.ebeogu@gmail.com>
2022-04-07 15:51:57 +0100

    reducer and context set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, useRoutes } from "react-router-dom";
 import LandingPage from "./components/landing/LandingPage";
+import Signup from "./components/User/Signup";
+import Signin from "./components/User/Signin";
 
 function AppRoutes() {
-  const routes = useRoutes([{ path: "/", element: <LandingPage /> }]);
+  const routes = useRoutes([
+    { path: "/", element: <LandingPage /> },
+    { path: "/buyer/signup", element: <Signup /> },
+    { path: "/buyer/signin", element: <Signin /> },
+  ]);
   return routes;
 }
 
---
+++ src/components/User/Signin.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../styles/user/user.css";
+import signImage from "../../assets/user/sign-in.png";
+import Navbar from "../generalComponents/Navbar";
+import Footer from "../generalComponents/Footer";
+
+const Signin = () => {
+  return (
+    <main>
+      <Navbar />
+      <section className="sign__main-container">
+        <div className="sign__form-section">
+          <h3 className="sign__txt">Sign In</h3>
+          <form action="" className="sign__form-group">
+            <label htmlFor="email address" className="sign__form-label">
+              Email Address
+            </label>
+            <input className="sign__form-input" type="email" />
+            <label htmlFor="password" className="sign__form-label">
+              Password
+            </label>
+            <input type="password" className="sign__form-input" />
+            <div className="sign__forgot-container">
+              <div>
+                <button className="sign__terms-btn"></button>
+
+                <span
+                  className="sign__accept-txt"
+                  style={{ paddingLeft: "0.65rem" }}
+                >
+                  Remember Me
+                </span>
+              </div>
+              <Link to="/" className="sign__terms-link">
+                Forgot Password
+              </Link>
+            </div>
+            <div className="sign__sign-btn-container">
+              <button type="submit" className="sign__up-btn">
+                SIGN IN
+              </button>
+            </div>
+
+            <div style={{ textAlign: "center" }}>
+              <span className="sign__account-txt">Don't have an account? </span>
+              <Link to="/buyer/signup" className="sign__link">
+                Sign Up
+              </Link>
+            </div>
+          </form>
+        </div>
+        <div className="sign__img-container">
+          <img src={signImage} alt="" />
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Signin;
