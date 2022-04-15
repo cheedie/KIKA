99c5d2839c24fc3ebdce9f94ca4967b8aade098b
Martin Karugaba <martinkarugaba21@gmail.com>
2022-04-15 18:20:33 +0300

    added man and woman section to home-page

---
+++ src/components/Product/Product.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../styles/Home/product.scss';
+
+const Product = () => {
+  return (
+    <section className="product">
+      <div>
+        <div className="poduct__info"></div>
+        <div className="product__price"></div>
+      </div>
+    </section>
+  );
+};
+
+export default Product;
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,18 +2,20 @@ import React from 'react';
 import Navbar from '../landing-page/Navbar';
 import Footer from '../landing-page/Footer';
 import '../../styles/Home/home.scss';
+import Product from '../Product/Product';
 
 const HomePage = () => {
   return (
     <section className="home">
       <Navbar />
+      {/* Hero */}
       <section className="home__hero">
         <div className="home__info">
           <h1>
             Welcome to <span>KIKA</span>
           </h1>
           <p className="sub__heading">Home of all thrift wears</p>
-          <p className='details'>
+          <p className="details">
             At KIKA we understand the importance of thrift wears and
             its availability to you and the need to get them at your
             comfort with just your smart device.
@@ -25,6 +27,30 @@ const HomePage = () => {
           <button>SHOP MEN</button>
         </div>
       </section>
+      {/* End hero */}
+
+      {/* man and woman section */}
+      <section className="man__and__woman">
+        <div className='images__container'>
+          <div className="man">
+            <div className="btn__container">
+              <button>SHOP WOMEN</button>
+            </div>
+          </div>
+          <div className="woman">
+            <div className="btn__container">
+              <button>SHOP MEN</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="home__products">
+          <Product/>
+          <Product/>
+          <Product/>
+        </div>
+      </section>
+      {/* End man and woman */}
       <Footer />
     </section>
   );
