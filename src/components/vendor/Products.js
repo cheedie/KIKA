53492188d8ffc6b4bfbbcb8fab79d827ab58b7e1
iamifechi <iamifechi@gmail.com>
2022-04-16 13:00:07 +0100

    add product WIP

--- src/components/vendor/Products.js
+++ src/components/vendor/Products.js
@@ -2,32 +2,32 @@ import shirt from "../../assets/vendor/images/shirt.png"
 import styled from "styled-components";
 import image from "../../assets/vendor/images/image_vector.png"
 import { StyledButton } from './Button.styled';
-import {StyledForm , StyledLabel, StyledInput } from './Form.style';
+import {StyledForm , StyledLabel, StyledInput ,Wrapper} from './Form.style';
 import {useFormik} from 'formik';
 import * as Yup from 'yup'
 
 export default function Products() {
   return (
-    <Container id="Container" className="products">
+    <div id="wrapper" className="products">
         <nav>
-            <Container>
+            <div>
             <a href="/" className="subtitle active">All (2)</a>
             <a href="/" className="subtitle">Online (0)</a>
             <a href="/" className="subtitle">Pending Review (0)</a>
             <a href="/" className="subtitle">Draft (0)</a>
-            </Container>
+            </div>
             <button className="button">Add Product</button>
         </nav>
-        <Container className="body">
-            <Container className="filters">
-            <Container className='dropdown'>
+        <div className="body">
+            <div className="filters">
+            <div className='dropdown'>
 
                     <select name="size">
                         <option value="">XX</option>
                         <option value="XL">XL</option>
                     </select>
                     <label htmlFor='Filter' className="button">Filter</label>
-                    </Container>
+                    </div>
                 <form>
                     <input 
                         name="search" 
@@ -42,20 +42,20 @@ export default function Products() {
                     />
                 </form>
 
-                <Container className='dropdown'>
+                <div className='dropdown'>
                     {/* <label htmlFor='Filter'>Filter</label> */}
                     <select name="size" placeholder='select'>
                         <option value=""> Select Category</option>
                         <option value="A">A-grade</option>
                         <option value="B">B-grade</option>
                     </select>
-                    </Container>
+                    </div>
 
-            </Container>
+            </div>
 
             
-        <Container className="product_list">
-            <Container className="product_titles">
+        <div className="product_list">
+            <div className="product_titles">
                 <p className="product_image_title">Image</p>
                 <p>Name</p>
                 <p>Status</p>
@@ -63,17 +63,17 @@ export default function Products() {
                 <p>Price</p>
                 <p>Views</p>
                 <p>Date</p>
-            </Container>
-            <Container className="products">
+            </div>
+            <div className="products">
                 <ProductTile/>
-            </Container>
+            </div>
 
-        </Container>
-        </Container>
+        </div>
+        </div>
         <Upload />
 
     
-    </Container>
+    </div>
   )
 }
 
@@ -114,17 +114,17 @@ function ProductTile() {
       {tile.map((item,index)=>{
           return(
             
-            <Container className="product_tile" key={`product-${index}`}>
-                <Container className="product_img">
+            <div className="product_tile" key={`product-${index}`}>
+                <div className="product_img">
                     <img src={item.image} alt={item.title}/>
-                </Container>
+                </div>
                 <p className="product_title">{item.title}</p>
                 <p className="product_status">{item.status}</p>
                 <p className="product_stock">{item.stock}</p>
                 <p className="product_price">{item.price}</p>
                 <p className="product_views">{item.views}</p>
                 <p className="product_date">{item.date}<br/>Last Modified</p>
-            </Container>
+            </div>
           )
       })}
       </>
@@ -135,40 +135,49 @@ function ProductTile() {
 
 function Upload () {
     return(
-        <UploadContainer>
+        <UploadWrapper>
             <UploadContainer>
                 <StyledForm>
-                    <Heading>Add New Product</Heading>
-                    <Container>
+                    <Wrapper>
+                        <Heading> + Add New Product</Heading>
+                    </Wrapper>
+                    
+                    <Wrapper>
                         <UploadInput type='file'/>
-                        <Container>
-                            <Input type='text' placeholder='A new product' />
-                            <Container>
-                                <Container>
-                                    <label>Price</label>
-                                    <Input type='text' placeholder='A new product' />
-                                </Container>
-                                <Container>
-                                    <label>Discounted Price</label>
-                                    <Input type='text' placeholder='A new product' />
-                                </Container>
+                        <Wrapper grid gap='2'>
+                            <Wrapper>
+                                <StyledLabel className="floating">A new product</StyledLabel>
+                                <StyledInput type='text' placeholder='A new product' floating/>
+                            </Wrapper>
+                            <Wrapper flex>
+                            <Wrapper>
+                                <StyledLabel className="floating">Price</StyledLabel>
+                                <StyledInput type='text' placeholder='A new product' floating/>
+                            </Wrapper>
+                            <Wrapper>
+                                <StyledLabel className="floating">Discounted Price</StyledLabel>
+                                <StyledInput type='text' placeholder='A new product' floating/>
+                            </Wrapper>
 
-                            </Container>
-                        </Container>
+                            </Wrapper>
+                        </Wrapper>
 
-                    </Container>
+                    </Wrapper>
 
-                    <Container>
-                        <Input type='text' placeholder='A new product' />
-                        <Input type='text' placeholder='A new product' />
-                        <Input type='text' placeholder='A new product' />
-                    </Container>
+                    <Wrapper>
+                        <Wrapper>
+                            <StyledLabel className="floating">Select category</StyledLabel>
+                            <StyledInput type='text' placeholder='A new product' floating/>
+                        </Wrapper>
+                        
+                       
+                    </Wrapper>
 
-                        <StyledButton type='submit' >Add Product</StyledButton>
+                        <StyledButton type='submit' label ='Add Product'/>
                    
                 </StyledForm>
             </UploadContainer>
-        </UploadContainer>
+        </UploadWrapper>
 
     )    
 }
