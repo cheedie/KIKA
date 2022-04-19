f2ce893a604635b2b81dcbe2eb7d9886ec0ec26d
teelda <matildaipeh@gmail.com>
2022-04-19 21:13:46 +0100

    cart page

--- src/components/StaticPages/Condition/Policy.js
+++ src/components/StaticPages/Condition/Policy.js
@@ -1,11 +1,91 @@
 import React from 'react'
-import "../../../styles/StaticStyles/OurPolicy.css";
+import "../../../styles/StaticStyles/Conditions.css";
 
-
-
-const Policy = () => {
+const Policy = ({
+  policycaption1,
+  onebody,
+  policycaption2,
+  twobody,
+  twobullet1,
+  twobullet2,
+  twobullet3,
+  policycaption3,
+  threebody,
+  threebody1,
+  threebullet1,
+  threebullet2,
+  threebullet3,
+  threebullet4,
+  policycaption4,
+  fourbody,
+  fourbullet1,
+  fourbullet2,
+  fourbullet3,
+  fourbullet4,
+  fourbullet5,
+  fourbullet6,
+  fourbullet7,
+  fourbullet8,
+  policycaption5,
+  fivebody,
+  fivebullet1,
+  fivebullet2,
+  fivebullet3,
+  fivebullet4,
+  fivebody1,
+  fivebullet5,
+  fivebullet6,
+}) => {
   return (
-    <div>Policy</div>
+    <>
+      <section className='conditions_container'>
+            <div className='conditions_row'>
+                <div id='sectionone'>
+                    <h1>{policycaption1}</h1>
+                    <p>{onebody}</p>
+                </div> 
+                <div id='sectiontwo'>
+                    <h1>{policycaption2}</h1>
+                    <p>{twobody}</p>
+                    <p id='bullet'> {twobullet1}</p>
+                    <p id='bullet'> {twobullet2}</p>
+                    <p id='bullet'> {twobullet3}</p>
+                </div> 
+                <div id='sectionthree'>
+                    <h1>{policycaption3}</h1>
+                    <p>{threebody}</p>
+                    <p>{threebody1}</p>
+                    <p id='bullet'>{threebullet1}</p>
+                    <p id='bullet'>{threebullet2}</p>
+                    <p id='bullet'>{threebullet3}</p>
+                    <p id='bullet'>{threebullet4}</p>
+                </div> 
+                <div id='sectionfour'>
+                    <h1>{policycaption4}</h1>
+                    <p>{fourbody}</p>
+                    <p id='bullet'>{fourbullet1}</p>
+                    <p id='bullet'>{fourbullet2}</p>
+                    <p id='bullet'>{fourbullet3}</p>
+                    <p id='bullet'>{fourbullet4}</p>
+                    <p id='bullet'>{fourbullet5}</p>
+                    <p id='bullet'>{fourbullet6}</p>
+                    <p id='bullet'>{fourbullet7}</p>
+                    <p id='bullet'>{fourbullet8}</p>
+                </div>
+                <div id='sectionfive'>
+                    <h1>{policycaption5}</h1>
+                    <p>{fivebody}</p>
+                    <p id='bullet'>{fivebullet1}</p>
+                    <p id='bullet'>{fivebullet2}</p>
+                    <p id='bullet'>{fivebullet3}</p>
+                    <p id='bullet'>{fivebullet4}</p>
+                    <p>{fivebody1}</p>
+                    <p id='bullet'>{fivebullet5}</p> 
+                    <p id='bullet'>{fivebullet6}</p>
+                </div>
+            </div>    
+        </section>
+    </>
   )
 }
 
--- src/data/StaticPages/PolicyData.js
+++ src/data/StaticPages/PolicyData.js
@@ -1,3 +1,41 @@
 export const policyObjOne = {
-    
+    policycaption1: "1. About this Notice",
+    onebody: "This Privacy and Cookie Notice provides information on how KIKA collects and processes your personal data when you visit our website or mobile applications.",
+    policycaption2: "2. The Data We Collect About You?",
+    twobody: "We collect your personal data in order to provide and continually improve our products and services. We may collect, use, store and transfer the following different kinds of personal data about you: Here for marketing and personal data optimization purposes. KIKA also uses Google Digital Marketing to propose targeted offers. To find out more:",
+    twobullet1: ". Information you provide to us: We receive and store the information you provide to us including your identity data, contact data, delivery address and financial data.",
+    twobullet2: ". Information on your use of our website and/or mobile applications: We automatically collect and store certain types of information regarding your use of the KIKA marketplace including information about your searches, views, downloads and purchases.",
+    twobullet3: ". Information from third parties and publicly available sources: We may receive information about you from third parties including our carriers; payment service providers; merchants/brands; and advertising service providers.",
+    policycaption3: "3. Cookies and How We Use Them",
+    threebody:"A cookie is a small file of letters and numbers that we put on your computer if you agree.",
+    threebody1: "Cookies allow us to distinguish you from other users of our website and mobile applications, which helps us to provide you with an enhanced browsing experience. For example we use cookies for the following purposes:",
+    threebullet1: ". Recognising and counting the number of visitors and to see how visitors move around the site when they are using it (this helps us to improve the way our website works, for example by ensuring that users can find what they are looking for).",
+    threebullet2: ". Identifying your preferences and subscriptions e.g. language settings, saved items, items stored in your basket and Prime membership; and",
+    threebullet3: ". Sending you newsletters and commercial/advertising messages tailored to your interests.",
+    threebullet4: ". Our approved third parties may also set cookies when you use our marketplace. Third parties include search engines, providers of measurement and analytics services, social media networks and advertising companies.",
+    policycaption4: "4. How We Use Your Personal Data",
+    fourbody: "We use your personal data to operate, provide, develop and improve the products and services that we offer, including the following:",
+    fourbullet1: ". Registering you as a new customer.",
+    fourbullet2: ". Processing and delivering your orders.",
+    fourbullet3: ". Managing your relationship with us.",
+    fourbullet4: ". Enabling you to participate in promotions, competitions and surveys.",
+    fourbullet5: ". Improving our website, applications, products and services",
+    fourbullet6: ". Recommending/advertising products or services which may be of interest to you.",
+    fourbullet7: ". Complying with our legal obligations, including verifying your identity where necessary.",
+    fourbullet8: ". Detecting fraud.",
+    policycaption5: "5. How We Share Your Personal Data",
+    fivebody: "We may need to share your personal data with third parties for the following purposes:",
+    fivebullet1: ". Sale of products and services: In order to deliver your products and services purchased on our marketplace from third parties, we may be required to provide your personal data to such third parties.",
+    fivebullet2: ". Working with third party service providers: We engage third parties to perform certain functions on our behalf. Examples include fulfilling orders for products or services, delivering packages, analyzing data, providing marketing assistance, processing payments, transmitting content, assessing and managing credit risk, and providing customer service.",
+    fivebullet3: ". Business transfers: As we continue to develop our business, we might sell or buy other businesses or services. In such transactions, customer information may be transferred together with other business assets.",
+    fivebullet4: ". Detecting fraud and abuse: We release account and other personal data to other companies and organizations for fraud protection and credit risk reduction, and to comply with the law.",
+    fivebody1: "When we share your personal data with third parties we:",
+    fivebullet5: ". require them to agree to use your data in accordance with the terms of this Privacy and Cookie Notice, our Privacy Policy and in accordance with the law; and",
+    fivebullet6: ". only permit them to process your personal data for specified purposes and in accordance with our instructions. We do not allow our third-party service providers to use your personal data for their own purposes.",
 }
+
+
+
+
+
+
