6f23066ea7730eaa19b1f02a1d0688eedaba9581
Ifechi <iamifechi@gmail.com>
2022-05-03 09:55:42 +0100

    WIP

--- src/components/vendor/Products.js
+++ src/components/vendor/Products.js
@@ -10,7 +10,7 @@ import { useVendorContext } from "../../context/vendor_context";
 export default function Products({vendor, loading, error, products, refresh}) {
     const [isUploading, setUpload] = useState(false)
       useEffect(() => {
-        console.log("vendor", vendor)
+        //console.log("vendor", vendor)
         refresh(vendor._id);
       }, []);
 
